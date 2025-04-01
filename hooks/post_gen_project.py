be56f9ec1cbd26792e2c8c1d12be7a188ed84a33
Nathan Capon <nathan.capon@hygeos.com>
2025-04-01 15:02:12 +0200

    Fixs: Some corrections

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 import os
 import shutil
+import subprocess
 
+from pathlib import Path
 
 def delete_resource(resource):
     if os.path.isfile(resource):
@@ -11,12 +13,16 @@ def delete_resource(resource):
         # print("removing directory: {}".format(resource))
         shutil.rmtree(resource)
 
+subprocess.run(f'git init -b main .', shell=True)
+subprocess.run('git add README.md && git commit -m "First commit"', shell=True)
+
 # {% if cookiecutter.license  == "Unlicense" %}
 delete_resource('LICENCE.txt')
 # {% endif %}
 
 # {% if not cookiecutter.tests %}
 delete_resource('tests')
+delete_resource('pytest.ini')
 # {% endif %}
 
 print("> {{cookiecutter.project_name}} has been succesfully created !")
